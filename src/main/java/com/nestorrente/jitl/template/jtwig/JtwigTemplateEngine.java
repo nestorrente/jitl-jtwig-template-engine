16f7561b04f34d431c6f9871bdec2cf5650aa5fa
nestorrente <nestorrente@gmail.com>
2017-03-28 15:20:57 +0200

    Updated to JITL 3.0.0

--- src/main/java/com/nestorrente/jitl/template/jtwig/JtwigTemplateEngine.java
+++ src/main/java/com/nestorrente/jitl/template/jtwig/JtwigTemplateEngine.java
@@ -26,11 +26,6 @@ public class JtwigTemplateEngine implements TemplateEngine {
 		return this.render(JtwigTemplate.inlineTemplate(templateContents, this.config), parameters);
 	}
 
-	@Override
-	public String renderResource(String templateResource, Map<String, Object> parameters) {
-		return this.render(JtwigTemplate.classpathTemplate(templateResource, this.config), parameters);
-	}
-
 	private String render(JtwigTemplate template, Map<String, Object> parameters) {
 		return template.render(JtwigModel.newModel(parameters));
 	}
