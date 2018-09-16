85e6d0614e0c259c9e4055652d6e800ec0d7b9b7
nestorrente <nestorrente@gmail.com>
2018-09-16 02:11:58 +0200

    Changed old @Param to new @ParamName

--- src/test/java/com/nestorrente/jitl/HtmlViewLoader.java
+++ src/test/java/com/nestorrente/jitl/HtmlViewLoader.java
@@ -1,11 +1,11 @@
 package com.nestorrente.jitl;
 
-import com.nestorrente.jitl.annotation.param.Param;
+import com.nestorrente.jitl.annotation.param.ParamName;
 
 import java.util.List;
 
 public interface HtmlViewLoader {
 
-	String getUsersView(@Param("users") List<String> userList);
+	String getUsersView(@ParamName("users") List<String> userList);
 
 }
